e20249150f40c1509ff608961038d90ec9d665b5
Siddhant Kadam <siddhantkadam044@gmail.com>
2025-03-18 11:42:30 +0530

    remove duplicate codes from repo

--- Arrays/Sorts/SelectionSort.java
+++ Arrays/Sorts/SelectionSort.java
@@ -2,31 +2,26 @@ package Arrays.Sorts;
 
 import java.util.Arrays;
 
-public class SelectionSort
-{
-    public static void doSelectionSort(int[] a)
-    {
+public class SelectionSort {
+    public static void doSelectionSort(int[] a) {
         int temp = 0;
-        for(int i =0;i<a.length;i++)
-        {
+        for (int i = 0; i < a.length; i++) {
             int minIndex = i;
-            for(int j=i+1;j<a.length;j++)
-            {
-                if(a[j]<a[minIndex])
-                {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[minIndex]) {
                     minIndex = j;
                 }
             }
-            if(i!=minIndex)
-            {
+            if (i != minIndex) {
                 temp = a[i];
                 a[i] = a[minIndex];
                 a[minIndex] = temp;
             }
         }
     }
+
     public static void main(String[] args) {
-        int[] a = {8,5,1,9,4,7,2,3};
+        int[] a = {8, 5, 1, 9, 4, 7, 2, 3};
         doSelectionSort(a);
         System.out.println(Arrays.toString(a));
     }
